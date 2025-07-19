f2a11693eb3546e87df97346672c7548963885f6
kosugi <tomo@cry.ptomeria.com>
2025-07-20 04:29:00 +0900

    remove WM_KEYDOWN to terminate event

--- src/main.rs
+++ src/main.rs
@@ -170,8 +170,7 @@ unsafe extern "system" fn wndproc(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: 
             EndPaint(hwnd, &ps);
             LRESULT(0)
         }
-        WM_KEYDOWN | WM_CHAR | WM_LBUTTONDOWN | WM_RBUTTONDOWN | WM_MBUTTONDOWN
-        | WM_XBUTTONDOWN => {
+        WM_CHAR | WM_LBUTTONDOWN | WM_RBUTTONDOWN | WM_MBUTTONDOWN | WM_XBUTTONDOWN => {
             let _ = DestroyWindow(hwnd);
             LRESULT(0)
         }
